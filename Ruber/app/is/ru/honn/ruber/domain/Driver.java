8d4bcab8bd53029538f5865bfac221af13fe5baa
kobblander <kobblander@gmail.com>
2014-10-24 16:38:52 +0000

    Driver domain class

---
+++ Ruber/app/is/ru/honn/ruber/domain/Driver.java
@@ -0,0 +1,100 @@
+package is.ru.honn.ruber.domain;
+
+/**
+ * <h1>Driver</h1>
+ * <h2>is.ru.honn.ruber.domain</h2>
+ * <p></p>
+ * Created on 24.10.2014.
+ *
+ * @author jakob
+ * @version 1.1
+ */
+public class Driver {
+
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+
+    public Driver() {
+    }
+
+    public Driver(int id, String username, String firstName, String lastName, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Driver(String username, String firstName, String lastName, String password, String email) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
