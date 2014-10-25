6b76d12701dfc63b5a86d8bdf41d8221ecfc3f59
kobblander <kobblander@gmail.com>
2014-10-25 15:42:56 +0000

    Added test clients.

---
+++ Ruber/app/is/ru/honn/ruber/test/gateway/GatewayTestClient.java
@@ -0,0 +1,16 @@
+package is.ru.honn.ruber.test.gateway;
+
+/**
+ * <h1>GatewayTestClient</h1>
+ * <h2>is.ru.honn.ruber.test.gateway</h2>
+ * <p></p>
+ * Created on 25.10.2014.
+ *
+ * @author jakob
+ * @version 1.1
+ */
+public class GatewayTestClient {
+
+    GatewayTestClient() {
+    }
+}
---
+++ Ruber/app/is/ru/honn/ruber/test/service/ServiceTestClient.java
@@ -0,0 +1,16 @@
+package is.ru.honn.ruber.test.service;
+
+/**
+ * <h1>ServiceTestClient</h1>
+ * <h2>is.ru.honn.ruber.test.service</h2>
+ * <p></p>
+ * Created on 25.10.2014.
+ *
+ * @author jakob
+ * @version 1.1
+ */
+public class ServiceTestClient {
+
+    ServiceTestClient() {
+    }
+}
